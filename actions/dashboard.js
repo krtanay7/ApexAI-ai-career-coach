3828a499a7b17c6171af878139e08b77594098f2
Kumar Tanay <krtanay97@gmail.com>
2026-01-28 11:41:23 +0530

    .

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -6,6 +6,11 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.gemini_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+console.log(
+  "GEMINI KEY LENGTH:",
+  process.env.GEMINI_API_KEY?.length
+);
+
 
 export const generateAIInsights = async (industry) => {
   const prompt = `
