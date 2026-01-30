8220c2599aca32cdb14219450d1c36cba19323e7
Kumar Tanay <krtanay97@gmail.com>
2026-01-30 15:50:10 +0530

    testimonial

--- data/testimonial.js
+++ data/testimonial.js
@@ -7,14 +7,14 @@ export const testimonial = [
     role: "Software Engineer",
     company: "Tech Giant Co.",
   },
- {
-  quote:
-    "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
-  author: "Aditya Sharma",
-  image: "https://www.vecteezy.com/photo/49174246-a-smiling-young-indian-man-with-formal-shirts-outdoors",
-  role: "Product Manager",
-  company: "StartUp Inc.",
-},
+  {
+    quote:
+      "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
+    author: "Aditya Sharma",
+    image: "https://randomuser.me/api/portraits/men/32.jpg",
+    role: "Product Manager",
+    company: "StartUp Inc.",
+  },
   {
     quote:
       "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
